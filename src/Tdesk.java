6163a82cf25083a238e3fad6d10c4ecf01b1a82d
Pavel Trostianko <rengetsu@mail.ru>
2020-02-11 18:10:21 +0200

    Add files via upload
    Project created

---
+++ src/Tdesk.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author raimo
+ */
+public class Tdesk {
+    
+   
+    public boolean chLogin(String email, String password){
+       
+        if(email.equals("rai@gmail.com") && password.equals("123"))
+            return true;
+      return false;
+    }
+}
